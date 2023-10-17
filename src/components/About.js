1d6b6acac23cd6872121d7c4679a6ca89af9e129
alexsylvian <lazeratus1@gmail.com>
2023-10-16 22:34:25 -0400

    props lab passed

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,29 @@
 import React from "react";
+import user from "../data/user";
+import Links from "./Links";
 
-function About() {
-  return (
+function About(props) {
+  console.log(props)
+
+  if(!props.bio) {
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        {/* <p>{props.bio}</p> */}
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        <Links />
+      </div>
+    )
+  } else {
+      return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links />
     </div>
-  );
+    )
+  }
 }
 
 export default About;
